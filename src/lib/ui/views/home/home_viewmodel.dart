f74398dbb753296b117bc153e47c05af2193d222
akbar904 <zharfan@walturn.com>
2024-10-13 15:25:04 +0700

    Update files

--- src/lib/ui/views/home/home_viewmodel.dart
+++ src/lib/ui/views/home/home_viewmodel.dart
@@ -1,36 +1,36 @@
 
-import 'package:my_app/app/app.bottomsheets.dart';
-import 'package:my_app/app/app.dialogs.dart';
 import 'package:my_app/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:my_app/services/sp500_service.dart';
 
 class HomeViewModel extends BaseViewModel {
 	final _dialogService = locator<DialogService>();
 	final _bottomSheetService = locator<BottomSheetService>();
+	final _sp500Service = locator<SP500Service>();
 
-	String get counterLabel => 'Counter is: $_counter';
+	Map<String, dynamic> _sp500Metrics = {};
+	Map<String, dynamic> get sp500Metrics => _sp500Metrics;
 
-	int _counter = 0;
-
-	void incrementCounter() {
-		_counter += 4;
-		rebuildUi();
+	Future<void> fetchSP500Metrics() async {
+		setBusy(true);
+		_sp500Metrics = await _sp500Service.getMockData();
+		setBusy(false);
 	}
 
 	void showDialog() {
 		_dialogService.showCustomDialog(
 			variant: DialogType.infoAlert,
-			title: 'Steve Rocks!',
-			description: 'Give steve $_counter stars on Github',
+			title: 'S&P 500 Metrics',
+			description: 'Here are some mock metrics: $_sp500Metrics',
 		);
 	}
 
 	void showBottomSheet() {
 		_bottomSheetService.showCustomSheet(
 			variant: BottomSheetType.notice,
-			title: 'title',
-			description: 'desc',
+			title: 'S&P 500 Notice',
+			description: 'Mock data fetched successfully.',
 		);
 	}
 }
