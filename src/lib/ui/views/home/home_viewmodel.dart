edad11110dff94dac0e0ab221f4b9f13543d7389
akbar904 <zharfan@walturn.com>
2024-10-13 12:06:23 +0700

    Update files

--- src/lib/ui/views/home/home_viewmodel.dart
+++ src/lib/ui/views/home/home_viewmodel.dart
@@ -4,33 +4,34 @@ import 'package:my_app/app/app.dialogs.dart';
 import 'package:my_app/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:my_app/services/financial_service.dart';
 
 class HomeViewModel extends BaseViewModel {
 	final _dialogService = locator<DialogService>();
 	final _bottomSheetService = locator<BottomSheetService>();
+	final _financialService = locator<FinancialService>();
 
-	String get counterLabel => 'Counter is: $_counter';
+	Map<String, dynamic>? sp500Metrics;
 
-	int _counter = 0;
-
-	void incrementCounter() {
-		_counter += 4;
-		rebuildUi();
+	Future<void> fetchSP500Metrics() async {
+		setBusy(true);
+		sp500Metrics = await _financialService.getSP500Metrics();
+		setBusy(false);
 	}
 
 	void showDialog() {
 		_dialogService.showCustomDialog(
 			variant: DialogType.infoAlert,
-			title: 'Steve Rocks!',
-			description: 'Give steve $_counter stars on Github',
+			title: 'S&P 500 Metrics',
+			description: sp500Metrics != null ? sp500Metrics.toString() : 'No data available',
 		);
 	}
 
 	void showBottomSheet() {
 		_bottomSheetService.showCustomSheet(
 			variant: BottomSheetType.notice,
-			title: 'title',
-			description: 'desc',
+			title: 'S&P 500 Metrics',
+			description: sp500Metrics != null ? sp500Metrics.toString() : 'No data available',
 		);
 	}
 }
